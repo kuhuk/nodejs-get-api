f067b762649e031bbc2159c49a91c54716d58953
Kuhuk Sharma <kuhuk@github.com>
2019-08-30 12:50:45 +0530

    get and post apis working

--- app.js
+++ app.js
@@ -1,8 +1,41 @@
 import express from 'express';
 import db from './db/db';
+import bodyParser from 'body-parser';
 
 // Set up the express app
 const app = express();
+
+// Parse incoming requests data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.post('/api/v1/todos', (req, res) => {
+    console.log(req.body);
+    if(!req.body.title) {
+        return res.status(400).send({
+            success : 'false',
+            message : 'title is required'
+        });
+    } else if(!req.body.description) {
+        return res.status(400).send({
+            success : 'false',
+            message : 'description is required'
+        });
+    }
+
+    const todo = {
+        id : db.length + 1,
+        title : req.body.title,
+        description : req.body.description
+    }
+
+    db.push(todo);
+
+    return res.status(201).send({
+        success : 'true',
+        message : 'todo added successfully'
+    })
+});
 
 // get all todos
 app.get('/api/todos', (req, res) => {
